b612d8bae859a7085192345538794a65cba9d565
PepseeActus <christophe.sbastien@gmail.com>
2018-08-09 13:56:47 +0200

    Initial commit

---
+++ js/portfolio.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  /*MODAL VIDEO*/
+  var videoSrc;
+  $('.video-btn').click(function() {
+      videoSrc = $(this).data( "src" );
+  });
+  $('#myModal').on('shown.bs.modal', function (e) {
+  $("#video").attr('src',videoSrc + "?rel=0&amp;showinfo=0&amp;modestbranding=1&amp;autoplay=1" );
+  })
+  $('#myModal').on('hide.bs.modal', function (e) {
+      $("#video").attr('src',videoSrc);
+  })
+});
